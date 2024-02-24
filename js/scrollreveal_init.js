7480594edd606c748bb13669f9a4b491fbdbab3b
nkietikul <161115613+nkietikul@users.noreply.github.com>
2024-02-24 01:12:54 -0700

    Add files via upload

---
+++ js/scrollreveal_init.js
@@ -0,0 +1,15 @@
+  window.sr = ScrollReveal({
+    reset:false,
+    scale: 1,
+    distance: '10%',
+    duration: 1200,
+    delay: 30,
+    easing: 'ease',
+  }
+  );
+
+    sr.reveal('.reveal');
+    sr.reveal('.reveal2', {
+    	delay:60,
+    }
+    );
